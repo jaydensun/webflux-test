d4d6362817aadd5cff2b403f8f79bd1609613896
tandysun <tandysun@tencent.com>
2022-01-10 18:23:59 +0800

    Initial commit

---
+++ src/main/java/org/spring/springboot/webflux/controller/CityWebFluxController.java
@@ -0,0 +1,45 @@
+package org.spring.springboot.webflux.controller;
+
+import org.spring.springboot.domain.City;
+import org.spring.springboot.handler.CityHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping(value = "/city")
+public class CityWebFluxController {
+
+    @Autowired
+    private CityHandler cityHandler;
+
+    @GetMapping(value = "/{id}")
+    public Mono<City> findCityById(@PathVariable("id") Long id) {
+        City city = new City();
+        city.setId(id);
+        city.setCityName("city" + id);
+        return Mono.just(city);
+//        return cityHandler.findCityById(id);
+    }
+
+    @GetMapping()
+    public Flux<City> findAllCity() {
+        return cityHandler.findAllCity();
+    }
+
+    @PostMapping()
+    public Mono<Long> saveCity(@RequestBody City city) {
+        return cityHandler.save(city);
+    }
+
+    @PutMapping()
+    public Mono<Long> modifyCity(@RequestBody City city) {
+        return cityHandler.modifyCity(city);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public Mono<Long> deleteCity(@PathVariable("id") Long id) {
+        return cityHandler.deleteCity(id);
+    }
+}
